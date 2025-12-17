51a7bac3ec1ef9531db6e35f51dcfbfed02b78b8
Paweł Dybiec <pawel@dybiec.info>
2025-12-17 21:20:56 +0100

    nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let pkgs = import nixpkgs{system="x86_64-linux";};
+    in{
+    devShells.x86_64-linux.default = pkgs.mkShell{
+      name = "bla";
+      buildInputs = with pkgs;[(agda.withPackages [agdaPackages.standard-library])];
+    };
+  };
+}
+
