170c9825b908ab2d828c087a4416136cc51905d1
Paweł Dybiec <pawel@dybiec.info>
2025-12-18 13:32:05 +0100

    nix pkg

--- flake.nix
+++ flake.nix
@@ -6,7 +6,15 @@
 
   outputs = { self, nixpkgs }: 
     let pkgs = import nixpkgs{system="x86_64-linux";};
+    mgr = pkgs.agdaPackages.mkDerivation {
+      version = "0.1";
+      pname = "mgr";
+      src = ./.;
+      buildInputs = [ pkgs.agdaPackages.standard-library ];
+      meta = {};
+    };
     in{
+    packages.x86_64-linux.default = mgr;
     devShells.x86_64-linux.default = pkgs.mkShell{
       name = "bla";
       buildInputs = with pkgs;[(agda.withPackages [agdaPackages.standard-library])];
