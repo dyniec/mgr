57f4e87b8dd755035e6ce9cf2584b5f0a700e559
Paweł Dybiec <pawel@dybiec.info>
2026-02-17 20:10:27 +0100

    Build pdf with nix

--- flake.nix
+++ flake.nix
@@ -9,12 +9,23 @@
     mgr = pkgs.agdaPackages.mkDerivation {
       version = "0.1";
       pname = "mgr";
-      src = ./.;
+      src = ./mgr;
       buildInputs = [ pkgs.agdaPackages.standard-library ];
+      meta = {};
+    };
+    doc = with pkgs; stdenv.mkDerivation {
+      version = "0.1";
+      pname = "doc";
+      src = ./.;
+      buildInputs = [ texliveFull pandoc python313Packages.pandoc-latex-environment (agda.withPackages [agdaPackages.standard-library])];
+      buildPhase = "make doc.pdf";
+      installPhase = "mkdir -p $out ;cp doc.pdf $out/";
       meta = {};
     };
     in{
     packages.x86_64-linux.default = mgr;
+    packages.x86_64-linux.mgr = mgr;
+    packages.x86_64-linux.doc = doc;
     devShells.x86_64-linux.default = pkgs.mkShell{
       name = "bla";
       buildInputs = with pkgs;[
